606bbfaf68b5963d2251ef445f703517fcca3046
rahuldhillo <49671524+rahuldhillo@users.noreply.github.com>
2023-01-16 21:02:13 +0530

    1 file modified, 1 added, 1 deleted

---
+++ App.jsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { useState } from 'react';
+
+import './style.css';
+
+function App(props) {
+  const [count, setRandomCount] = useState(function generateRandomInteger() {
+    return Math.floor(Math.random() * 100);
+  });
+
+  function clickHandler(e) {
+    setRandomCount(Math.floor(Math.random() * 100));
+  }
+
+  return (
+    <div style={{ margin: 'auto', width: 100, display: 'block' }}>
+      <h1> {count} </h1>
+      <p>
+        <button onClick={clickHandler}> Click </button>
+      </p>
+    </div>
+  );
+}
+
+export default App;
